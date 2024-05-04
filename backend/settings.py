3b35237bc6a0c3de608d843233cafdfbed299116
hoetaek <rhoetaek@gmail.com>
2024-05-04 21:00:01 +0900

    update static location and csrf trusted origin

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,11 @@ CORS_ALLOWED_ORIGINS = env.list(
     default=["http://127.0.0.1:3000", "http://localhost:3000"],
 )
 
+CSRF_TRUSTED_ORIGINS = env.list(
+    "CSRF_TRUSTED_ORIGINS",
+    default=["http://127.0.0.1:3000", "http://localhost:3000"],
+)
+
 
 # Application definition
 
@@ -137,8 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
