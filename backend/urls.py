8f92019b4d5f496a9aa363c2d3dadbe9b7449595
행복한엄쌤 <sendmethere@naver.com>
2024-05-05 00:16:38 +0900

    API URL Changed

--- backend/urls.py
+++ backend/urls.py
@@ -5,28 +5,12 @@ from players.views import PlayerViewSet
 from quizzes.views import QuizViewSet, PlayerSuccessRecordsView, add_success_user, get_random_quizzes, get_player_statistics
 
 router = DefaultRouter()
+router.register(r'players', PlayerViewSet)
+router.register(r'quizzes', QuizViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/players/', include(PlayerViewSet.as_view({
-        'get': 'list',    # 'GET' 요청
-        'post': 'create'  # 'POST' 요청
-    }))),
-    path('api/players/<int:pk>/', include(PlayerViewSet.as_view({
-        'get': 'retrieve',   # 'GET' 요청
-        'put': 'update',     # 'PUT' 요청
-        'delete': 'destroy'  # 'DELETE' 요청
-    }))),
-    path('api/quizzes/', include(QuizViewSet.as_view({
-        'get': 'list',   # 'GET' 요청
-        'post': 'create' # 'POST' 요청
-    }))),
-    path('api/quizzes/<int:pk>/', include(QuizViewSet.as_view({
-        'get': 'retrieve',   # 'GET' 요청
-        'put': 'update',     # 'PUT' 요청
-        'delete': 'destroy'  # 'DELETE' 요청
-    }))),
     path('api/success-records/<str:nickname>/', PlayerSuccessRecordsView.as_view(), name='player-success-records'),
     path('api/success_user', add_success_user, name='add_success_user'),
     path('api/random-quizzes/', get_random_quizzes, name='random-quizzes'),
